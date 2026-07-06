b5e49c2b9b54952966e5992943664e6a8c2abfce
Daniel <88160656+danieljustus@users.noreply.github.com>
2026-07-06 16:23:03 +0200

    feat(version): integrate versionkit and add --json flag (#71)

--- cmd/symfritz/version.go
+++ cmd/symfritz/version.go
@@ -8,15 +8,21 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/danieljustus/symaira-corekit/updatecheck"
+	"github.com/danieljustus/symaira-corekit/versionkit"
 )
 
 func newVersionCmd() *cobra.Command {
 	var check bool
+	var flagJSON bool
 	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Print version",
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			fmt.Println("symfritz", version)
+			info := versionkit.New("symfritz", version, 1)
+			if flagJSON {
+				return info.Write(cmd.OutOrStdout())
+			}
+			fmt.Fprintln(cmd.OutOrStdout(), info.String())
 			if check {
 				checker := updatecheck.NewChecker("danieljustus", "symaira-fritz")
 				release, err := checker.Check(context.Background(), version)
@@ -34,5 +40,6 @@ func newVersionCmd() *cobra.Command {
 		},
 	}
 	cmd.Flags().BoolVar(&check, "check", false, "Check for updates on GitHub")
+	cmd.Flags().BoolVar(&flagJSON, "json", false, "Emit version as machine-readable JSON")
 	return cmd
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/danieljustus/symaira-fritz
 go 1.26.4
 
 require (
-	github.com/danieljustus/symaira-corekit v0.2.1
+	github.com/danieljustus/symaira-corekit v0.3.0
 	github.com/spf13/cobra v1.10.2
 	golang.org/x/term v0.44.0
 )
@@ -12,6 +12,5 @@ require (
 	github.com/BurntSushi/toml v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
-	golang.org/x/crypto v0.53.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
 )
