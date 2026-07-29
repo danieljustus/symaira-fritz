7ba8a3ffe1ce82ba099c05e4e131395ab0547eb0
Daniel <justus.daniel@web.de>
2026-07-29 16:39:46 +0200

    chore(release): bump version to v0.4.0
    Mechanical version fix from prerelease gate: main.go 0.2.0 → 0.4.0

--- cmd/symfritz/main.go
+++ cmd/symfritz/main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/danieljustus/symaira-fritz/internal/secret"
 )
 
-var version = "0.2.0"
+var version = "0.4.0"
 
 func main() {
 	slog.SetDefault(logkit.NewFromEnv("symfritz"))
